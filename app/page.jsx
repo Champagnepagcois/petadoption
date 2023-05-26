747e4cc9e91da702db9e3527cba9b04936b559c6
Marlon <marlonrodriguez2b@gmail.com>
2023-05-26 13:28:07 -0600

    none

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <section className="section-index">
+        <div className="index-background-image">
+          
+        </div>
+      </section>
+    </div>
+  );
+}
