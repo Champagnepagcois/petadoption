5b428316f84aa54a3454afecb66ead7550d21b44
Marlon <marlonrodriguez2b@gmail.com>
2023-06-07 01:23:10 -0600

    Se configuro archivo para tener las rutas privadas, de esta manera solo puedes acceder a estas si ya iniciaste sesion.

---
+++ middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(request) {
+  const jwt = request.cookies.get("MyTokenName");
+  if (request.nextUrl.pathname.includes("/User")) {
+    if (jwt === undefined) {
+      return NextResponse.redirect(new URL("/Auth", request.url));
+    }
+    try {
+      const { payload } = await jwtVerify(
+        jwt.value,
+        new TextEncoder().encode("secret")
+      );
+      console.log(payload);
+      return NextResponse.next();
+    } catch (error) {
+      console.log(error);
+      return NextResponse.redirect(new URL("/Auth", request.url));
+    }
+  }
+  return NextResponse.next();
+}
