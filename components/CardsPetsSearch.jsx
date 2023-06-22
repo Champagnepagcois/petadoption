a669e8b3054b2e109c2a33a289b919bab8dc050f
Marlon <marlonrodriguez2b@gmail.com>
2023-06-22 01:18:33 -0600

    New component Card pet added

--- components/CardsPetsSearch.jsx
+++ components/CardsPetsSearch.jsx
@@ -1,8 +1,14 @@
+'use client';
+import axios from "axios";
 import React from "react";
 
 export default function CardsPetsSearch() {
+  const handleRedirectPet=async()=>{
+    const response = await axios.get("api/detailsPet");
+    console.log(response);
+  }
   return (
-    <div className="itemcontent">
+    <div onClick={() => handleRedirectPet()} className="itemcontent">
       <div className="itemcontent-image">
         <img
           src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=662&q=80"
